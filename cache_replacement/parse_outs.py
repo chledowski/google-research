1a99c8d57db9c624a37377dadadc0881c2f0bde3
chledowski <jakub.chledowski@gmail.com>
2020-11-20 10:59:59 +0100

    add parse outs file

--- cache_replacement/parse_outs.py
+++ cache_replacement/parse_outs.py
@@ -2,6 +2,11 @@ import argparse
 import os
 
 
+def reader(filename):
+    for row in open(filename, "r"):
+        yield row.strip
+
+
 def parse_outs(exp_folder, pred_f, evict_f):
 
     pred_file = os.path.join(exp_folder, 'predictions', pred_f)
@@ -9,19 +14,36 @@ def parse_outs(exp_folder, pred_f, evict_f):
     output_file = os.path.join(exp_folder, 'parsed_output', evict_f)
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-    with open(pred_file, 'r') as f_p:
-        with open(evict_file, 'r') as f_e:
-            with open(output_file, 'w') as f_o:
-                for line in f_p:
-                    if line.strip() == "":
-                        print(1)
-                predictions = f_p.readlines()
-                evictions = f_e.readlines()
-                for line in predictions[:30]:
-                    print(line.strip())
-                print()
-                for line in evictions[:2]:
-                    print(line.strip())
+    pred_reader = reader(pred_file)
+    evict_reader = reader(evict_file)
+
+    print(pred_reader)
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    print(next(pred_reader))
+    # with open(pred_file, 'r') as f_p:
+    #     with open(evict_file, 'r') as f_e:
+    #         with open(output_file, 'w') as f_o:
+    #             for line in f_p:
+    #                 print(line.strip())
+    #                 if line.strip() == "":
+    #                     print(1)
+    #                     break
+    #             # predictions = f_p.readlines()
+    #             evictions = f_e.readlines()
+    #             # for line in predictions[:30]:
+    #             #     print(line.strip())
+    #             print()
+    #             for line in evictions[:2]:
+    #                 print(line.strip())
 
 
 if __name__ == "__main__":
